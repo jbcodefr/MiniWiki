63cbc41d4ad60f615dbed2ed3c7552a059ef06de
jbcode <78531307+jbcodefr@users.noreply.github.com>
2021-03-06 21:55:33 +0100

    adding User and Article class with db

---
+++ src/main/java/com/jbcode/miniwiki/beans/Article.java
@@ -0,0 +1,69 @@
+package com.jbcode.miniwiki.beans;
+
+import java.util.Objects;
+
+public class Article {
+    private Long id;
+    private String title;
+    private String content;
+
+    public Article() {
+    }
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(id, article.id) && Objects.equals(title, article.title) && Objects.equals(content, article.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, content);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/jbcode/miniwiki/dao/SingletonConnection.java
@@ -0,0 +1,28 @@
+package com.jbcode.miniwiki.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingletonConnection {
+    private static Connection connection;
+
+    static {
+        try{
+            Class.forName("org.postgresql.Driver");
+            connection= DriverManager.getConnection(
+                 "jdbc:postgresql://localhost:5432/miniwiki",
+                 "postgres"   ,
+                    "toor"
+
+            );
+
+        }catch (Exception e)
+        {
+            System.out.println(" problem avec la connection");
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection(){
+        return connection;
+    }
+}
