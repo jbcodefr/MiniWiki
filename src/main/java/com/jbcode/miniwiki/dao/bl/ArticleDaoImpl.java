3991b3070f910237c808f097d6601199b9ee9875
jbcode <78531307+jbcodefr@users.noreply.github.com>
2021-03-07 20:39:02 +0100

    implement ArticleDaoImpl

--- src/main/java/com/jbcode/miniwiki/dao/bl/ArticleDaoImpl.java
+++ src/main/java/com/jbcode/miniwiki/dao/bl/ArticleDaoImpl.java
@@ -7,15 +7,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleDaoImpl implements IArticleDao {
 
 
     @Override
-    public Article save(Article article) {
+    public  Article save(Article article) {
         Connection conn= SingletonConnection.getConnection();
-
       try{
           PreparedStatement ps=conn.prepareStatement(
                   "insert into article( title, content) values (?,?)"
@@ -34,27 +34,72 @@ public class ArticleDaoImpl implements IArticleDao {
     @Override
     public List<Article> articlebyword(String word) {
         Connection conn= SingletonConnection.getConnection();
-
-        return null;
+        List<Article> articleList=new ArrayList<Article>();
+        try{
+            PreparedStatement ps= conn.prepareStatement(
+                    "select * from article where title like ? or content like ?");
+            ps.setString(1,"%"+word+"%");
+            ps.setString(2,"%"+word+"%");
+            ResultSet rs=ps.executeQuery();
+            while (rs.next()){
+                Article p=new Article();
+                p.setId(rs.getLong("id"));
+                p.setTitle(rs.getString("title"));
+                p.setContent(rs.getString("content"));
+                articleList.add(p);
+            }
+        }catch (SQLException e ){
+            e.printStackTrace();
+        }
+        return articleList;
     }
 
     @Override
     public Article getArticle(Long articleId) {
         Connection conn= SingletonConnection.getConnection();
 
-        return null;
+        Article p=new Article();
+        try{
+            PreparedStatement ps=conn.prepareStatement("select *from article where id = ?");
+            ps.setLong(1,articleId);
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                p.setId(rs.getLong("id"));
+                p.setTitle(rs.getString("title"));
+                p.setContent(rs.getString("content"));
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return p;
     }
 
     @Override
     public Article updateArticle(Article article) {
         Connection conn= SingletonConnection.getConnection();
-
-        return null;
+        try {
+            PreparedStatement ps=conn.prepareStatement("UPDATE article set title=?,content=? where id=?");
+            ps.setString(1, article.getTitle());
+            ps.setString(2,article.getContent());
+            ps.setLong(3,article.getId());
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return article;
     }
 
     @Override
     public void delete(Long articleId) {
         Connection conn= SingletonConnection.getConnection();
-
+        try {
+            PreparedStatement ps=conn.prepareStatement("DELETE from article where id=?");
+            ps.setLong(1,articleId);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 }
--- src/test/java/com/jbcode/miniwiki/dao/bl/ArticleDaoImplTest.java
+++ src/test/java/com/jbcode/miniwiki/dao/bl/ArticleDaoImplTest.java
@@ -1,16 +1,29 @@
 package com.jbcode.miniwiki.dao.bl;
 
 import com.jbcode.miniwiki.beans.Article;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArticleDaoImplTest {
-    public static void main(String[] args) {
-        ArticleDaoImpl articleDao=new ArticleDaoImpl();
-        try{
-            Article article=articleDao.save(new Article("mon stitre","dsdufksdjfbskdfjksdf kjdsbf kjbds kjbf sd"));
-            System.out.println("added"+article);
-        }catch (Exception e){
-        e.printStackTrace();
+
+    @Test
+    void save() {
     }
-}}
+
+    @Test
+    void articlebyword() {
+    }
+
+    @Test
+    void getArticle() {
+    }
+
+    @Test
+    void updateArticle() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
